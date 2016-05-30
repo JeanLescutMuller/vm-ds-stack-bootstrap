b07c511894e13707fc24532134f4dbeb76597f74
Jean Lescut <Jean.lescut@gmail.com>
2016-05-30 18:56:52 +0200

    adding ssl certificates

--- basis.sh
+++ basis.sh
@@ -1,4 +1,7 @@
-USERNAME="johndoe"
+# Configuration :
+USERNAME="johndoe" # username to be created
+EMAIL="johndoe@example.com" # email of the username (for ssl certificate)
+FQDN="example.com" # for ssl certificate
 
 # Add port 1935 to ssh, to be able to bypass the outbound firewall of some companies
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config_default
@@ -23,4 +26,22 @@ dpkg-reconfigure -f noninteractive tzdata
 
 # Print colors in the prompt
 ./color_prompt.sh
+
+# Creating SSL key and Certificates for furhter HTTPS service
+cd /usr/local/bin
+git clone https://github.com/certbot/certbot
+cd certbot
+./certbot-auto certonly -d $FQDN -m $EMAIL --standalone --agree-tos
+KEY_PATH="/etc/letsencrypt/live/$FQDN/privkey.pem"
+CERT_PATH="/etc/letsencrypt/live/$FQDN/fullchain.pem"
+echo '5 3  * *  * root /usr/local/bin/certbot/certbot-auto renew --standalone --pre-hook "service nginx stop" --post-hook "service nginx start" -q' >> /etc/crontab
+
+# Or self-signed (non-trusted) certificate :
+# sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/my_key.pem -out /etc/ssl/my_cert.pem
+# KEY_PATH="/etc/ssl/my_key.pem"
+# CERT_PATH="/etc/ssl/my_cert.pem"
+
+
+
+
 
