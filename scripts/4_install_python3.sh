85d9a139515cab8ca64f50989b46d1d6b15d8f66
Jean <Jean.lescut@gmail.com>
2017-02-10 15:47:08 +0100

    Update script to Anaconda 3.4

--- scripts/4_install_python3.sh
+++ scripts/4_install_python3.sh
@@ -5,6 +5,9 @@
 
 # Si nécessaire : sudo s
 
-wget http://repo.continuum.io/archive/Anaconda3-4.0.0-Linux-x86_64.sh
+apt-get install -y bzip2 # Sinon ca crash
+wget https://repo.continuum.io/archive/Anaconda3-4.3.0-Linux-x86_64.sh
 sudo bash Anaconda3-4.0.0-Linux-x86_64.sh
+# Suivre instruction... (tout par défaut)
+
 rm Anaconda3-4.0.0-Linux-x86_64.sh
