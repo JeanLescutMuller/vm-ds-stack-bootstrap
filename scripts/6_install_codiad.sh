397198ff73b2dda78a3aa98a9fe49088271abeff
Jean <Jean.lescut@gmail.com>
2017-02-10 17:32:55 +0100

    Change Codiad to codiad and add comments

--- scripts/6_install_codiad.sh
+++ scripts/6_install_codiad.sh
@@ -6,11 +6,14 @@ sudo apt-get install php5-common php5-cli php5-fpm
 scripts/0_uninstalling_apache2.sh
 sudo service nginx restart
 
-sudo git clone https://github.com/Codiad/Codiad /var/www/html/
+sudo git clone https://github.com/Codiad/Codiad /var/www/html/codiad
 sudo git clone https://github.com/Fluidbyte/Codiad-Terminal.git /var/www/html/Codiad/plugins/
-sudo cp /var/www/html/Codiad/config.example.php /var/www/html/Codiad/config.php
+sudo cp /var/www/html/codiad/config.example.php /var/www/html/codiad/config.php
 sudo chown www-data:www-data -R /var/www/html/
 
+# On the first page :
+# poject name : (whatever you want, for example "main")
+# project path : (something under /var/www/html/codiad, for example /var/www/html/codiad/project)
 
 
 
