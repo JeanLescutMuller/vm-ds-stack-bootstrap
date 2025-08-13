1b1304b97ec2e5e0a737eca284708db451520845
Jean Lescut-Muller <jean.lescut@gmail.com>
2025-08-13 22:17:25 +0000

    some changes and bugfixes

--- 04_jupyterhub/root/opt/anaconda3/etc/jupyter/jupyter_lab_config.py
+++ 04_jupyterhub/root/opt/anaconda3/etc/jupyter/jupyter_lab_config.py
@@ -24,9 +24,14 @@ c.ServerApp.allow_origin = '*'
 # c.ServerApp.allow_remote_access = True # Probably not needed
 c.ServerApp.allow_root = True
 c.ServerApp.base_url = '/jupyter/'
-c.ServerApp.iopub_data_rate_limit = 1000000 * 1000
+c.ServerApp.iopub_data_rate_limit = 10_000_000_000
 # I should probably change the above to: c.ServerApp.limit_rate = False
 c.ServerApp.ip = '0.0.0.0'
 c.ServerApp.port = 8000 # Not sure if it's needed
 c.ServerApp.port_retries = 0
 c.ServerApp.root_dir = '/home/enrices'
+c.ServerApp.iopub_msg_rate_limit = 10000.0
+# The hash below has been generated using `/opt/anaconda3/bin/python -c "from jupyter_server.auth import passwd; print(passwd())"`
+# This hash is very strong, and safe to push on a public Github
+c.ServerApp.password = 'argon2:$argon2id$v=19$m=10240,t=10,p=8$4KRqMxQCM0mwu1xpJX2F9A$4YZx8KIogxe8RyYs2DIcDPwzxe5MQOfDhOk+0ItLT/A'
+
